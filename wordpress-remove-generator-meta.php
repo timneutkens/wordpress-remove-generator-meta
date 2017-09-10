22986d4f1ee801b36a27d03494a852ba07a87ba9
Tim Neutkens <timneutkens@me.com>
2017-09-10 15:50:45 +0200

    Clean up code

--- wordpress-remove-generator-meta.php
+++ wordpress-remove-generator-meta.php
@@ -1,6 +1,4 @@
 <?php
-namespace TimNeutkens\Wordpress\RemoveGeneratorMeta;
-
 /*
 Plugin Name:  Remove generator meta
 Description:  Remove generator meta tag from head
@@ -10,8 +8,4 @@ Author URI:   https://github.com/timneutkens
 License:      MIT License
 */
 
-function generator() {
-    return '';
-}
-
-add_filter('the_generator', __NAMESPACE__.'\\generator');
+add_filter('the_generator', '__return_false');
